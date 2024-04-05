222a8c5035950e1d73ce314aa8c6d84af298c5a6
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-05 00:18:40 -0300

    feat: criando a estrutura inicial da página login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+//style
+import "./styles.scss";
+
+import { useState } from "react";
+
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [sucess, setSucess] = useState("");
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8080/user/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setSucess(res.data.msg);
+        setError("");
+        setEmail("");
+        setPassword("");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.response.data);
+        setSucess("");
+      });
+  };
+
+  return (
+    <main>
+      <div id="login-register-container">
+        <div id="logo-and-description">
+          <picture id="logo-insta-container">
+            <img src="/instagram_logo.jpg" alt="Logo do instagram" />
+          </picture>
+          <p>Faça login para ver fotos e vídeos dos seus amigos.</p>
+        </div>
+        <a href="/register">Cadastre-se</a>
+        <div id="separate-form">
+          <span></span>OU<span></span>
+        </div>
+        <form id="form" onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value.toString())}
+            value={email}
+          ></input>
+          <input
+            type="text"
+            placeholder="Digite sua senha"
+            onChange={(e) => setPassword(e.target.value.toString())}
+            value={password}
+          ></input>
+          <p>
+            As pessoas que usam nosso serviço podem ter enviado suas informações
+            de contato para o Instagram.{" "}
+            <a
+              href="https://www.facebook.com/help/instagram/261704639352628"
+              target="_blank"
+            >
+              Saiba mais
+            </a>
+          </p>
+          <p>
+            Ao se cadastrar, você concorda com nossos{" "}
+            <a
+              href="https://help.instagram.com/581066165581870/?locale=pt_BR"
+              target="_blank"
+            >
+              Termos
+            </a>
+            ,{" "}
+            <a href="https://www.facebook.com/privacy/policy" target="_blank">
+              Política de Privacidade
+            </a>{" "}
+            e{" "}
+            <a
+              href="https://privacycenter.instagram.com/policies/cookies/"
+              target="_blank"
+            >
+              Política de Cookies
+            </a>
+            .
+          </p>
+          <div id="form-btn">
+            <button type="submit">Entrar</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
