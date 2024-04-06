135b43311fd41b31185332dfdd80a30982de98ea
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-06 11:52:02 -0300

    feat: adicionando componente Login na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Login from "@/components/login/Login";
 
 export default function Home() {
   return (
     <>
-      <main></main>
+      <main>
+        <Login />
+      </main>
     </>
   );
 }
